19e5377f8bbf101d13b583f5fc6d730f22d5c0b3
bichenul <nicolas@lenec.fr>
2017-01-12 11:32:15 +0100

    tutoriels_nodeJS
    tutoriels_nodeJS

---
+++ routes/serveur.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil, que puis-je pour vous ?');
+});
+
+app.get('/sous-sol', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+
+app.get('/etage/1/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hé ho, c\'est privé ici !');
+});
+
+//Nous pouvons maintenant déléguer la gestion de la vue (du HTML) à notre moteur de template. Plus besoin d'écrire du HTML au milieu du code JavaScript comme un cochon !
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.render('chambre.ejs', {etage: req.params.etagenum});
+});
+
+//hestion des erreurs 404
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
+app.listen(8080);
